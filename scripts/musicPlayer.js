4395cbc11823e8a44aa87d1334392c466f2c11a9
Gulshat Nugumanova <gulshatik.nugumanova84@gmail.com>
2021-03-30 23:56:42 +0300

    player created

---
+++ scripts/musicPlayer.js
@@ -0,0 +1,34 @@
+export const musicPlayerInit = () => {
+  const audio = document.querySelector('.audio');
+  const audioImg = document.querySelector('.audio-img');
+  const audioHeader = document.querySelector('.audio-header');
+  const audioPlayer = document.querySelector('.audio-player');
+  const audioNavigation = document.querySelector('.audio-navigation');
+  const audioButtonPlay = document.querySelector('.audio-button__play');
+  const audioProgress = document.querySelector('.audio-progress');
+  const audioProgressTiming = document.querySelector('.audio-progress__timing');
+  const audioTimePassed = document.querySelector('.audio-time__passed');
+  const audioTimeTotal = document.querySelector('.audio-time__total');
+
+  const playList = ['hello', 'flow', 'speed'];
+
+  let trackIndex = 0;
+
+  audioNavigation.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.classList.contains('audio-button__play')) {
+      audio.classList.toggle('play');
+      audioButtonPlay.classList.toggle('fa-play');
+      audioButtonPlay.classList.toggle('fa-pause');
+
+      if (audioPlayer.paused) {
+        audioPlayer.play();
+      } else audioPlayer.pause();
+    }
+
+    if (target.classList.contains('audio-button__prev'))
+
+    if (target.classList.contains('audio-button__next'))
+  });
+};
